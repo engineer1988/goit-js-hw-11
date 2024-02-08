bb4870e0ffcaf1f33c9c95f0331a9a39773db53c
Dmytro Maniuta <ip722428@gmail.com>
2024-02-08 21:00:22 +0200

    hw-11

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -2,22 +2,27 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import 'css-loader';
 
 const form = document.querySelector('.form');
 const button = document.querySelector('.button');
 const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+loader.classList.add('hide');
 
 button.addEventListener('click', () => {
+  loader.classList.remove('hide');
   fetchUsers()
     .then(images => renderUsers(images))
     .catch(error => console.log(error));
+  form.reset();
 });
 
 function fetchUsers() {
-  //   const search = form.search.value;
-  return fetch(
-    'https://pixabay.com/api/?key=42241202-737945e445eb9c5ec6bcca7e8&q=dog_type=photo'
-  ).then(response => {
+  const search = form.search.value;
+  loader.classList.add('loader');
+  const URL = `https://pixabay.com/api/?key=42241202-737945e445eb9c5ec6bcca7e8&q=${search}&image_type=photo&orientation=horizontal&safesearch=true`;
+  return fetch(URL).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
@@ -26,22 +31,25 @@ function fetchUsers() {
 }
 
 function renderUsers(images) {
-  const markup = images
+  const markup = images.hits
     .map(image => {
       return `
       <li class="gallery-item">
-      <a class="gallery-link" href="${image.largeImageURL}">
-        <img class="gallery-image" src="${image.previewURL}" alt="${image.pageURL}" />
+      <a class="gallery-link" href=${image.largeImageURL}>
+        <img class="gallery-image" src=${image.webformatURL} alt=${image.tags} />
       </a>
     </li>
       `;
     })
     .join('');
-  gallery.insertAdjacentHTML('beforeend', markup);
-}
-const options = {
-  captionsData: 'alt',
-  captionDelay: 250,
-};
+  gallery.innerHTML = '';
+  gallery.innerHTML = markup;
+  const options = {
+    captionsData: 'alt',
+    captionDelay: 250,
+  };
 
-const lightbox = new SimpleLightbox('.gallery a', options);
+  const lightbox = new SimpleLightbox('.gallery a', options);
+  lightbox.refresh();
+  loader.classList.add('hide');
+}
---
+++ src/js/test.js
@@ -0,0 +1,35 @@
+const fetchUsersBtn = document.querySelector('.btn');
+const userList = document.querySelector('.user-list');
+
+fetchUsersBtn.addEventListener('click', () => {
+  fetchUsers()
+    .then(users => renderUsers(users))
+    .catch(error => console.log(error));
+});
+
+function fetchUsers() {
+  return fetch(
+    'https://jsonplaceholder.typicode.com/users?_limit=7&_sort=name'
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
+
+function renderUsers(users) {
+  console.log(users);
+  const markup = users
+    .map(user => {
+      return `
+          <li>
+            <p><b>Name</b>: ${user.name}</p>
+            <p><b>Email</b>: ${user.email}</p>
+            <p><b>Company</b>: ${user.company.name}</p>
+          </li>
+      `;
+    })
+    .join('');
+  userList.insertAdjacentHTML('beforeend', markup);
+}
