6ef0a367353638056e89e511336a4b84d5412f89
engineer1988 <140731601+engineer1988@users.noreply.github.com>
2024-02-10 14:31:55 +0000

    Deploying to gh-pages from @ engineer1988/goit-js-hw-11@d09e82b095492be1041ec74a6b7358c39239e162 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,8 +1,25 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{i as l,S as c}from"./assets/vendor-16e51dcd.js";const i=document.querySelector(".form"),n=document.querySelector(".button"),s=document.querySelector(".gallery"),r=document.querySelector(".loader");r.classList.add("hide");n.addEventListener("click",()=>{r.classList.remove("hide"),h().then(e=>g(e)).catch(e=>console.log(e)),i.reset()});function h(){const e=i.search.value;r.classList.add("loader");const o=`https://pixabay.com/api/?key=42241202-737945e445eb9c5ec6bcca7e8&q=${e}&image_type=photo&orientation=horizontal&safesearch=true`;return fetch(o).then(t=>{if(!t.ok)throw new Error(t.status);return t.json()})}function g(e){e.hits.length===0&&l.error({backgroundColor:"rgb(239, 64, 64)",titleSize:"16px",titleColor:"rgb(250, 250, 251)",messageColor:"rgb(250, 250, 251)",title:"Error",position:"topRight",message:"Sorry, there are no images matching your search query."});const o=e.hits.map(a=>`
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{i as l,S as c}from"./assets/vendor-6b0e5f77.js";const o=document.querySelector(".form"),n=document.querySelector(".gallery"),i=document.querySelector(".loader");i.classList.add("hide");o.addEventListener("submit",e=>{e.preventDefault(),i.classList.remove("hide"),h(e).then(s=>m(s)).catch(s=>console.log(s)),o.reset()});function h(e){const s=e.target.elements.search.value;i.classList.add("loader");const r=`https://pixabay.com/api/?key=42241202-737945e445eb9c5ec6bcca7e8&q=${s}&image_type=photo&orientation=horizontal&safesearch=true`;return fetch(r).then(a=>{if(!a.ok)throw new Error(a.status);return a.json()})}function m(e){e.hits.length===0&&l.error({maxWidth:"432px",messageColor:"rgb(250, 250, 251)",messageSize:"16px",backgroundColor:"rgb(239, 64, 64)",position:"topRight",message:"Sorry, there are no images matching your search query. Please try again!"});const s=e.hits.map(t=>`
       <li class="gallery-item">
-      <a class="gallery-link" href=${a.largeImageURL}>
-        <img class="gallery-image" src=${a.webformatURL} alt=${a.tags} />
+      <a class="gallery-link" href="${t.largeImageURL}">
+        <img class="gallery-image" src="${t.webformatURL}" alt="${t.tags}" />
       </a>
-    </li>
-      `).join("");s.innerHTML="",s.innerHTML=o;const t={captionsData:"alt",captionDelay:250};new c(".gallery a",t).refresh(),r.classList.add("hide")}
+      <div class="image-text">
+       <div class="image-text-item"> 
+        <h2>Likes</h2>
+        <span class="span-size">${t.likes}</span>
+       </div> 
+       <div class="image-text-item"> 
+        <h2>Views</h2>
+        <span class="span-size">${t.views}</span>
+       </div> 
+       <div class="image-text-item"> 
+        <h2>Comments</h2>
+        <span class="span-size">${t.comments}</span>
+       </div> 
+       <div class="image-text-item"> 
+        <h2>Downloads</h2>
+        <span class="span-size">${t.downloads}</span>
+       </div> 
+      </div>
+    </li>`).join("");n.innerHTML="",n.innerHTML=s;const r={captionSelector:"img",captionsData:"alt",captionDelay:250};new c(".gallery a",r).refresh(),i.classList.add("hide")}
 //# sourceMappingURL=commonHelpers2.js.map
