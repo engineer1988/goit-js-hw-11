798590cdad3a76ad35566e5f54b70c7450bec7f5
Dmytro Maniuta <ip722428@gmail.com>
2024-02-16 13:42:13 +0200

    fixed mistakes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import 'css-loader';
 import fetchImages from './js/pixabay-api';
 import renderImages from './js/render-functions';
 
